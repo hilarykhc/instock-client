1e1a759b5223e1b2b20076bf1dc656e9902d862c
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-05 22:18:00 -0500

    install react-router-dom and routes

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        {/* <Route path="/" element={<WarehousePage />} />
+        <Route path="/warehouse" element={<WarehousePage />} />
+        <Route path="/warehouse/:id" element={<WarehouseDetail />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/inventory/:id" element={<InventoryDetail />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
