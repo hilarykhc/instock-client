2a4caf0706c52d1e3af39739ba6836ad312449d8
Hilary Chen <hilarychen@Hilarys-MacBook-Pro.local>
2024-02-05 22:41:42 -0500

    add assets folders and header logo and link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,15 @@
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import instockLogo from "../../assets/Logo/InStock-Logo_2x.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <section className="header__wrapper">
+        <Link to="/" className="header__link">
+          <img className="header__logo" src={instockLogo} alt="instock logo" />
+        </Link>
+      </section>
+    </header>
+  );
+}
