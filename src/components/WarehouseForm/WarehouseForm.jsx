8cbc115ca87f6e7370b313f6ae147477a192d18f
charuzain <charuabhizain@gmail.com>
2024-02-07 03:29:58 -0500

    Style form section and fix minor bug

--- src/components/WarehouseForm/WarehouseForm.jsx
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -7,19 +7,25 @@ const WarehouseForm = () => {
       <div className="warehouse-form__section-container">
         {/* Warehouse Detail Section */}
         <section className="warehouse-form-section section">
-          <h2 className='section__heading'>Warehouse Details</h2>
-          <div className="form__group">
-            <label htmlFor="warehouse_name">warehouse name</label>
+          <h2 className="section__heading">Warehouse Details</h2>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="warehouse_name">
+              warehouse name
+            </label>
             <input
+              className="warehouse-form__input"
               type="text"
               id="warehouse_name"
               name="warehouse_name"
               placeholder="Warehouse Name"
             />
           </div>
-          <div className="form__group">
-            <label htmlFor="address">street address</label>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="address">
+              street address
+            </label>
             <input
+              className="warehouse-form__input"
               type="text"
               id="address"
               name="address"
@@ -27,12 +33,22 @@ const WarehouseForm = () => {
             />
           </div>
 
-          <div className="form__group">
-            <label htmlFor="city">city</label>
-            <input type="text" id="city" name="city" placeholder="City" />
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="city">
+              city
+            </label>
+            <input
+              className="warehouse-form__input"
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City"
+            />
           </div>
-          <div className="form__group">
-            <label htmlFor="warehouse_name">Country</label>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="warehouse_name">
+              Country
+            </label>
             <input
               type="text"
               id="country"
@@ -44,37 +60,54 @@ const WarehouseForm = () => {
         <Divider />
         {/* Contact section */}
         <section className="warehouse-form-section section">
-          <h2 className='section__heading'>Contact Details</h2>
-          <div className="form__group">
-            <label htmlFor="contact_name">Contact Name</label>
+          <h2 className="section__heading">Contact Details</h2>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="contact_name">
+              Contact Name
+            </label>
             <input
+              className="warehouse-form__input"
               type="text"
               id="contact_name"
               name="contact_name"
               placeholder="Contact Name"
             />
           </div>
-          <div className="form__group">
-            <label htmlFor="contact_name">Position</label>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="contact_name">
+              Position
+            </label>
             <input
+              className="warehouse-form__input"
               type="text"
               id="contact_position"
               name="contact_position"
               placeholder="Position"
             />
           </div>
-          <div className="form__group">
-            <label htmlFor="contact_phone">Phone Number</label>
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="contact_phone">
+              Phone Number
+            </label>
             <input
+              className="warehouse-form__input"
               type="text"
               id="contact_phone"
               name="contact_phone"
               placeholder="Phone Number"
             />
           </div>
-          <div className="form__group">
-            <label htmlFor="email">Email</label>
-            <input type="text" id="email" name="email" placeholder="Email" />
+          <div className="warehouse-form__group">
+            <label className="warehouse-form__label" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="warehouse-form__input"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Email"
+            />
           </div>
         </section>
         <div className="warehouse-form__btn-container">
