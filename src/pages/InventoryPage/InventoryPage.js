0e4d71ac514dc633b16d410677fed9c539f6763c
charuzain <charuabhizain@gmail.com>
2024-02-08 22:02:25 -0500

    Make axios call to get all inventories

--- src/pages/InventoryPage/InventoryPage.js
+++ src/pages/InventoryPage/InventoryPage.js
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import InventoryListHeader from "../../components/InventoryListHeader/InventoryListHeader";
+import axios from "axios";
 
 const InventoryPage = () => {
+  const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+  
+  const fetchAllInventories = async() => {
+    try {
+      const response = await axios.get(`${REACT_APP_SERVER_URL}/inventories`)
+      console.log(response.data)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllInventories()
+  },[])
+
+
+
   return (
     <InventoryListHeader/>
   )
